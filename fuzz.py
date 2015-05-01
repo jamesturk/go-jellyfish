77b6f5d69a67dc25a7071979426be28ef8c59dfc
James Turk <james.p.turk@gmail.com>
2015-05-01 11:14:03 -0400

    fuzz.py

---
+++ fuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import csv
+import sys
+import os
+
+if __name__ == '__main__':
+    with open('testdata/{}.csv'.format(sys.argv[1])) as f:
+        reader = csv.reader(f)
+
+        for n, line in enumerate(reader):
+            with open('fuzz/{}-corpus/{}'.format(sys.argv[1], n), 'w') as out:
+                out.write(line[0])
+    os.system('go-fuzz-build github.com/jamesturk/go-jellyfish/fuzz/{}'.format(sys.argv[1]))
+    os.system('go-fuzz -bin=./{0}-fuzz -corpus=./fuzz/{0}-corpus -workdir=./fuzz/{0}-workdir'.format(sys.argv[1]))
--- fuzz.sh
+++
@@ -1,4 +0,0 @@
-go get github.com/dvyukov/go-fuzz/...
-cd fuzz
-go-fuzz-build github.com/jamesturk/go-jellyfish/fuzz/nysiis
-go-fuzz -bin=./nysiis-fuzz -corpus=./nysiis-corpus -workdir=./nysiis-workdir
